a285fdca72b73c7ac99e827369077ece341535e4
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-25 16:08:18 -0400

    personalizes contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,8 +22,8 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                344 E. 85th St. <br />
+                New York, NY 10028
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -31,12 +31,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                alx.vga2@email.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">646-897-8361</p>
             </div>
           </div>
         </div>
