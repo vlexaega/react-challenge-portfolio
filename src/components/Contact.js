8a00634dab58008fa033b7bb6ed7ecc55484aa3a
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-25 16:44:49 -0400

    this should fix the build issues

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export default function Contact() {
     }
 
     return (
-        <section id="contact" className="relative">
+    <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
@@ -64,9 +64,11 @@ export default function Contact() {
           </div>
         </div>
         <form
-          netlify
-          name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          name="contact" // Add the form name for Netlify form handling
+          data-netlify="true" // Add the data-netlify attribute for Netlify form handling
+          onSubmit={handleSubmit} // Added the onSubmit event handler here
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+            >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
