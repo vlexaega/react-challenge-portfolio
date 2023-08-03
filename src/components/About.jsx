e84cd57371f389b35cf44683e4cb1ac40d4c6eee
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-08-03 07:56:38 -0400

    adds github and linkedin links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function About() {
     return (
+      
+
+
       <section id="about">
+        
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center font-Georgia">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-5 md:mb-30 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
@@ -10,6 +14,33 @@ export default function About() {
             <br className="hidden lg:inline-block" />I love to build amazing
             apps.
           </h1>
+          <div className="flex justify-center mt-4">
+          {/* GitHub */}
+              <a
+              href="https://github.com/vlexaega"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              <img
+                  src='/github-mark.png'
+                  alt="GitHub"
+                  className="w-8 h-8 mx-2"
+              />
+          </a>
+          {/* LinkedIn */}
+          <a
+              href="https://www.linkedin.com/in/alexvega"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+              <img
+                  src='/LI-In-Bug.png'
+                  alt="LinkedIn"
+                  className="w-8 h-8 mx-2"
+              />
+          </a>
+      </div>
+      <br></br>
           <p className="mb-8 leading-relaxed">
           Hello World and welcome to my page! 
           <br></br>
