b1789cbd638d1c052958986b53d9ef5a152fb640
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-08-02 23:46:47 -0400

    testing new bg color

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className="text-gray-400 bg-gray-500 body-font">
       <Navbar />
       <About />
       <Projects />
